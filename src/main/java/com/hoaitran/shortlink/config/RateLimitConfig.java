4a42a40dcea61f15b4f78e280a968a98458d559c
TranHoai <tthoai654@gmail.com>
2026-05-16 13:46:12 +0700

    refactor: change redis port, host to url

--- src/main/java/com/hoaitran/shortlink/config/RateLimitConfig.java
+++ src/main/java/com/hoaitran/shortlink/config/RateLimitConfig.java
@@ -3,33 +3,40 @@ package com.hoaitran.shortlink.config;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.codec.ByteArrayCodec;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.redis.autoconfigure.DataRedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class RateLimitConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
+    @Autowired
+    private DataRedisProperties redisProperties;
 
     @Bean
     public RedisClient redisClient() {
-        return RedisClient.create(RedisURI.create(redisHost, redisPort));
+        // Nếu có chuỗi url gộp (giống trên Render), dùng url để khởi tạo kết nối hoàn
+        // chỉnh
+        if (redisProperties.getUrl() != null) {
+            return RedisClient.create(redisProperties.getUrl());
+        }
+        // Trường hợp chạy local hoặc các cấu hình lẻ, tự động build từ
+        // host/port/password có sẵn
+        return RedisClient.create(
+                io.lettuce.core.RedisURI.builder()
+                        .withHost(redisProperties.getHost())
+                        .withPort(redisProperties.getPort())
+                        .withPassword(redisProperties.getPassword().toCharArray())
+                        .withSsl(redisProperties.getSsl().isEnabled())
+                        .build());
     }
 
     @Bean
     public ProxyManager<byte[]> proxyManager(RedisClient redisClient) {
         StatefulRedisConnection<byte[], byte[]> connection = redisClient.connect(ByteArrayCodec.INSTANCE);
-        return LettuceBasedProxyManager.builderFor(connection)
-                // .withExpirationStrategy(ExpirationAfterWriteStrategy.basedOnTimeForRefilling(Duration.ofMinutes(1)))
-                .build();
+        return LettuceBasedProxyManager.builderFor(connection).build();
     }
 }
